452a64bec70553bf7cb9b74a270bb1f8b7f6b0e4
muhammedshameem33 <shameempp33@gmail.com>
2020-04-30 15:05:26 +0530

    [muhammed shameem] git add .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { RegistrationComponent } from './Component/registration/registration.component';
+import { LoginComponent } from './Component/login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
