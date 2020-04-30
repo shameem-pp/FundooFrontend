72d3134670be6db567eebc8bd1a0908a5141696b
muhammedshameem33 <shameempp33@gmail.com>
2020-04-30 20:10:07 +0530

    [muhammed shameem] git add .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,7 +16,8 @@ import { ForgotPasswordComponent } from './Component/forgot-password/forgot-pass
     RegistrationComponent,
     LoginComponent,
     ResetPasswordComponent,
-    ForgotPasswordComponent
+    ForgotPasswordComponent,
+    MatFormFieldModule
   ],
   imports: [
     BrowserModule,
