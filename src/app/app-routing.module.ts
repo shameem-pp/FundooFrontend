3f92fec435e17ed3394115df76b92f4de817e36a
muhammedshameem33 <shameempp33@gmail.com>
2020-05-01 20:32:56 +0530

    [muhammed shameem] git add .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { LoginComponent } from './Component/login/login.component';
+import { RegistrationComponent} from './Component/registration/registration.component';
+import { ResetPasswordComponent } from './Component/reset-password/reset-password.component';
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'registration',component:RegistrationComponent},
+  {path:'resetPassword',component:ResetPasswordComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
