da196a0d6118354623aaee6f01eaa9cc561d915f
muhammedshameem33 <shameempp33@gmail.com>
2020-05-01 11:32:09 +0530

    [muhammed shameem] git add .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
 import { RegistrationComponent } from './Component/registration/registration.component';
 import { LoginComponent } from './Component/login/login.component';
 import { ResetPasswordComponent } from './Component/reset-password/reset-password.component';
@@ -24,7 +23,6 @@ import { ForgotPasswordComponent } from './Component/forgot-password/forgot-pass
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule,
     MatFormFieldModule,
     MatCardModule,
     MatButtonModule
