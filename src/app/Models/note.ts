9b1ef2e1f3e0e50e1c8860fb1e0cb0c9f95a9365
muhammedshameem33 <shameempp33@gmail.com>
2020-05-11 00:54:06 +0530

    [muhammed shameem] git Refractor .

--- src/app/Models/note.ts
+++ src/app/Models/note.ts
@@ -1,5 +1,12 @@
 export class Note {
     title:string;
     description:string;
-    email:string
+    email:string;
+    archeive:boolean;
+    pin:boolean;
+    collaborator:string;
+    changeColor:string;
+    image:string;
+    remainder:string
+    trash:boolean;
 }
