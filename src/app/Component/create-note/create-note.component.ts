6c78834f514861c8bc22d703188edacd5dceded6
muhammedshameem33 <shameempp33@gmail.com>
2020-05-11 11:59:53 +0530

    [muhammed shameem] git Refractor .

--- src/app/Component/create-note/create-note.component.ts
+++ src/app/Component/create-note/create-note.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NoteService } from 'src/app/Service/note.service';
 import { Note } from 'src/app/Models/note';
+import { not } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-create-note',
@@ -28,8 +29,40 @@ export class CreateNoteComponent implements OnInit {
   }
 
   iconEvent(evnt){
-debugger;
+    switch(evnt['name']){
+    case "collaborator":this.collaborator(evnt['value']);
+    break;
+    case "color":this.addColor(evnt['value']);
+    break;
+    case "addImage":this.addImage(evnt['value']);
+    break;
+    case "archive":this.archieve(evnt['value']);
+    break;
+    case "reminder":this.addReminder(evnt['value']);
+    break;
+    }
   }
+
+  addReminder(eventValue) {
+    this.notes.remainder=eventValue;
+  }
+
+  archieve(eventValue) {
+    this.notes.archeive=eventValue;
+  }
+
+  addImage(eventValue) {
+    this.notes.image=eventValue;
+  }
+
+  addColor(eventValue) {
+    this.notes.changeColor=eventValue;
+  }
+
+  collaborator(eventValue) {
+    this.notes.collaborator=eventValue;
+  }
+ 
 
   ngOnInit(): void {
     this.notes=new Note();
