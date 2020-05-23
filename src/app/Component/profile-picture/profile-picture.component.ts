e42ac43f9ab0d7a2e021c34b0f9c7b1424937cbd
muhammedshameem33 <shameempp33@gmail.com>
2020-05-23 07:12:41 +0530

    [muhammed shameem] git Refractor .

--- src/app/Component/profile-picture/profile-picture.component.ts
+++ src/app/Component/profile-picture/profile-picture.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Optional, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { UserService } from 'src/app/Service/user.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-profile-picture',
@@ -11,7 +13,7 @@ export class ProfilePictureComponent implements OnInit {
   selectedFile: File;
 
   constructor(private service:UserService,public dialogRef: MatDialogRef<ProfilePictureComponent>,
-    @Optional() @Inject(MAT_DIALOG_DATA) public datas: any) { 
+    @Optional() @Inject(MAT_DIALOG_DATA) public datas: any,private spinner: NgxSpinnerService,private snackBar: MatSnackBar) { 
     }
 
 
@@ -23,15 +25,35 @@ export class ProfilePictureComponent implements OnInit {
   }
 
   uploadImage(){
+    this.startSpinner();
     const formData = new FormData();
     formData.append('file',this.selectedFile)
 
     this.service.uploadProfilePic("api/Account/UploadImage/"+localStorage.getItem('email'),formData).subscribe
     (
       response=>{
+        this.stopSpinner();
+        this.openSnackBar("Image Uploaded Successfully","Dismiss");
+        this.dialogRef.close();
         localStorage.setItem('profilePic',response['profilePic'].toString());
         this.selectedFile=null;
+      },
+      err=>{
+        this.stopSpinner();
+        this.openSnackBar("Failed","Dismiss");
       }
     )
+  }
+  startSpinner(){
+    this.spinner.show();
+  }
+  
+  stopSpinner(){
+    this.spinner.hide();
+  }
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+    });
   }
 }
