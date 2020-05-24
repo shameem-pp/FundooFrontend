a48095533115cdfd451335d7349a0194fed1e894
muhammedshameem33 <shameempp33@gmail.com>
2020-05-25 01:15:10 +0530

    [muhammed shameem] git Refractor .

--- src/app/Component/create-note/create-note.component.ts
+++ src/app/Component/create-note/create-note.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { NoteService } from 'src/app/Service/note.service';
 import { Note } from 'src/app/Models/note';
 import { Label } from 'src/app/Models/label';
-import { LabelNote } from 'src/app/Models/label-note';
-import { LabelService } from 'src/app/Service/label.service';
 
 @Component({
   selector: 'app-create-note',
@@ -11,104 +9,91 @@ import { LabelService } from 'src/app/Service/label.service';
   styleUrls: ['./create-note.component.css']
 })
 export class CreateNoteComponent implements OnInit {
-  clicked:boolean=false;
-  contents:any;
-  @Input() notes:Note;
-  @Input() labels:any
-  backgroundColor:any='rgb(255,255,255)';
-  labelArray=new Array();
-  data:LabelNote=new LabelNote();
-  constructor(private service:NoteService,private labelService:LabelService) { }
+  clicked: boolean = false;
+  contents: any;
+  @Input() notes: Note;
+  @Input() labels: any
+  backgroundColor: any = 'rgb(255,255,255)';
+  labelArray = new Array();
+  constructor(private service: NoteService) { }
 
-  @Output() notify=new EventEmitter<any>();
-  openNote(){
-    this.clicked=true;
+  @Output() notify = new EventEmitter<any>();
+  openNote() {
+    this.clicked = true;
   }
 
-  apiCallCreateNote(){
-
-    if(this.notes.title!=null || this.notes.description!=null){
-      this.service.createNote(this.notes,'api/Note/CreateNote').subscribe(
-        response=>{
-          if(this.labelArray.length!=0){
-            this.labelArray.forEach(element => {
-              this.addLabel(element['id'],response['id'])
-            });
-          }
-          this.clicked=false;
-         this.notify.emit({name:'callGetAllNoteApi'});
-          this.notes.title=null;
-          this.notes.description=null;
-          this.backgroundColor='rgb(255,255,255)';
+  apiCallCreateNote() {
+    if (this.notes.title != null || this.notes.description != null) {
+      this.service.createNote(this.notes, 'api/Note/CreateNote').subscribe(
+        response => {
+          this.clicked = false;
+          this.notify.emit({ name: 'callGetAllNoteApi' });
+          this.notes.title = null;
+          this.notes.description = null;
+          this.backgroundColor = 'rgb(255,255,255)';
         });
     }
-    else{
-      if(this.labelArray.length!=0){
-        this.labelArray=[];
+    else {
+      if (this.labelArray.length != 0) {
+        this.labelArray = [];
       }
-      this.clicked=false;
+      if(this.notes.remainder!=null){
+        this.notes.remainder=null;
+      }
+      this.clicked = false;
     }
   }
 
-  iconEvent(evnt){
-    switch(evnt['name']){
-    case "collaborator":this.collaborator(evnt['value']);
-    break;
-    case "color":this.addColor(evnt);
-    break;
-    case "addImage":this.addImage(evnt);
-    break;
-    case "archive":this.archieve(evnt['value']);
-    break;
-    case "reminder":this.addReminder(evnt['value']);
-    break;
-    case "createNote":this.apiCallCreateNote();
-    break;
-    case "label":this.addLabelToLabelArr(evnt);
-    break;
+  iconEvent(evnt) {
+    switch (evnt['name']) {
+      case "collaborator": this.collaborator(evnt['value']);
+        break;
+      case "color": this.addColor(evnt);
+        break;
+      case "addImage": this.addImage(evnt);
+        break;
+      case "archive": this.archieve(evnt['value']);
+        break;
+      case "reminder": this.addReminder(evnt['value']);
+        break;
+      case "createNote": this.apiCallCreateNote();
+        break;
+      case "label": this.addLabel(evnt);
+        break;
     }
   }
 
-  
- async addLabel(labelId,noteId) {
-    this.data.labelId=labelId;
-    this.data.noteId=noteId;
-    await this.labelService.createLabel('api/label/CreateLabelNote',this.data).subscribe
-    (
-      res=>{
-        debugger
-        console.log(res);
-      }
-    )
-  }
-  addLabelToLabelArr(evnt) {
+  addLabel(evnt) {
     this.labelArray.push(evnt.value);
   }
 
   addReminder(eventValue) {
-    this.notes.remainder=eventValue;
+    this.notes.remainder = eventValue;
   }
 
   archieve(eventValue) {
-    this.notes.archeive=eventValue;
+    this.notes.archeive = eventValue;
   }
 
   addImage(eventValue) {
-    this.notes.image=eventValue;
+    this.notes.image = eventValue;
   }
 
   addColor(eventValue) {
-      this.backgroundColor=eventValue.value.color;
-      this.notes.changeColor=eventValue.value.color;
+    this.backgroundColor = eventValue.value.color;
+    this.notes.changeColor = eventValue.value.color;
   }
 
   collaborator(eventValue) {
-    this.notes.collaborator=eventValue;
+    this.notes.collaborator = eventValue;
   }
- 
-
+  deleteLabel(index){
+    this.labelArray.splice(index,1);
+  }
+  deleteReminder(){
+    this.notes.remainder=null;
+  }
   ngOnInit(): void {
-    this.notes=new Note();
+    this.notes = new Note();
   }
-
 }
